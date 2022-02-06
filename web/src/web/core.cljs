cc1da284c5a5e38e9abacb3cab1c7946b68065f1
Gene Nakagaki <genenakagaki@Genes-MacBook-Air.local>
2022-02-06 17:50:33 +0900

    feat: lein new re-frame web +10x +kondo +cider +test

---
+++ web/src/web/core.cljs
@@ -0,0 +1,24 @@
+(ns web.core
+  (:require
+   [reagent.dom :as rdom]
+   [re-frame.core :as re-frame]
+   [web.events :as events]
+   [web.views :as views]
+   [web.config :as config]
+   ))
+
+
+(defn dev-setup []
+  (when config/debug?
+    (println "dev mode")))
+
+(defn ^:dev/after-load mount-root []
+  (re-frame/clear-subscription-cache!)
+  (let [root-el (.getElementById js/document "app")]
+    (rdom/unmount-component-at-node root-el)
+    (rdom/render [views/main-panel] root-el)))
+
+(defn init []
+  (re-frame/dispatch-sync [::events/initialize-db])
+  (dev-setup)
+  (mount-root))
